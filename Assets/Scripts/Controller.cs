a31647eaa8ba183b7a3eb97a24d51db38e3925d0
ViliusKk <vilius.kuokstas@gmail.com>
2025-01-12 17:10:34 +0200

    Wheel turn animation

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     public Rigidbody rb;
+    public GameObject carModel;
 
     public float forwardAcceleration = 10f;
     public float reverseAcceleration = 5f;
     public float maxSpeed = 50f;
     public float turnAngle = 200f;
     public float gravityForce = 10f;
+    public float dragOnGround = 3f;
 
     float speedInput;
 
@@ -20,6 +22,10 @@ public class Controller : MonoBehaviour
     public LayerMask ground;
     public float groundRayLength = 0.5f;
     public Transform groundRay;
+
+    public Transform wheelFL;
+    public Transform wheelFR;
+    public float maxWheelTurn = 45f;
 
     void Start()
     {
@@ -35,16 +41,42 @@ public class Controller : MonoBehaviour
 
         turnInput = Input.GetAxis("Horizontal");
 
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnAngle * Time.deltaTime *Input.GetAxis("Vertical"), 0f));
+        if (onGround)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnAngle * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
+        }
+
+        wheelFL.localRotation = Quaternion.Euler(wheelFL.localRotation.eulerAngles.x, turnInput * maxWheelTurn, wheelFL.localRotation.eulerAngles.z);
+        wheelFR.localRotation = Quaternion.Euler(wheelFR.localRotation.eulerAngles.x, turnInput * maxWheelTurn, wheelFR.localRotation.eulerAngles.z);
 
         transform.position = rb.transform.position;
     }
 
     void FixedUpdate()
     {
-        if (Mathf.Abs(speedInput) > 0)
+        onGround = false;
+        RaycastHit hit;
+
+        if(Physics.Raycast(groundRay.position, -transform.up, out hit, groundRayLength, ground))
         {
-            rb.AddForce(transform.forward * speedInput);
+            onGround = true;
+
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+            rb.transform.rotation = transform.rotation;
+        }
+
+        if (onGround)
+        {
+            rb.drag = dragOnGround;
+            if (Mathf.Abs(speedInput) > 0)
+            {
+                rb.AddForce(transform.forward * speedInput);
+            }
+        }
+        else
+        {
+            rb.drag = 0.1f;
+            rb.AddForce(Vector3.up * -gravityForce * 100f);
         }
     }
 }
